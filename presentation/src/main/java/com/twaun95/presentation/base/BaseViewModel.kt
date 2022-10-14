cab7ee5dfa7fc2da9ce8cd4cc0fe462af9f51bd9
taewaun <taewaunkwon@gmail.com>
2022-10-14 23:04:32 +0900

    feat : base loading

--- presentation/src/main/java/com/twaun95/presentation/base/BaseViewModel.kt
+++ presentation/src/main/java/com/twaun95/presentation/base/BaseViewModel.kt
@@ -1,7 +1,24 @@
 package com.twaun95.presentation.base
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 
 abstract class BaseViewModel : ViewModel() {
 
+    protected val _loading = MutableStateFlow(false)
+    val loading: StateFlow<Boolean>
+        get() = _loading
+
+    private val _error = MutableSharedFlow<String>()
+    val error: SharedFlow<String>
+        get() = _error
+
+    protected fun startLoading() { _loading.value = true }
+    protected fun stopLoading() { _loading.value = false }
+    protected fun setLoading(isLoading: Boolean) {
+        _loading.value = isLoading
+    }
 }
