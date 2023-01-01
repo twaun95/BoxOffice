2ac11ec86040eadc15702b1cd4041b98b168f746
taewaun <taewaunkwon@gmail.com>
2023-01-01 17:35:14 +0900

    feat : 독립적인 모듈을 위해 app 모듈의 유스케이스 의존성 주입 클래스를 domain 모듈로 이동

--- domain/src/main/java/com/twaun95/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/twaun95/domain/di/UseCaseModule.kt
@@ -1,4 +1,4 @@
-package com.twaun95.cleanarchitecture_hilt_kts.di
+package com.twaun95.domain.di
 
 import com.twaun95.domain.repository.MovieRepository
 import com.twaun95.domain.usecase.GetBoxOfficeUseCase
@@ -8,7 +8,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
