6703f4eaf2ee0ed260c35b3aa5a364828e8d7153
taewaun <taewaunkwon@gmail.com>
2023-01-01 17:33:14 +0900

    feat : 독립적인 모듈을 위해 app 모듈의 레파지토리 의존성 주입 클래스를 data 모듈로 이동

--- data/src/main/java/com/twaun95/data/di/RepositoryModule.kt
+++ data/src/main/java/com/twaun95/data/di/RepositoryModule.kt
@@ -1,4 +1,4 @@
-package com.twaun95.cleanarchitecture_hilt_kts.di
+package com.twaun95.data.di
 
 import com.twaun95.data.repository.MovieRepositoryImpl
 import com.twaun95.domain.repository.MovieRepository
